54241159f90c55922bb91e6d6815021b090eef06
arielarreola <arielyohaina.arreolablanco@umg.edu.mx>
2021-02-24 02:15:33 -0600

    version1

---
+++ index.js
@@ -0,0 +1,17 @@
+import "firebase/analytics";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCzYMgozRAhcArYzgUOiVXXhowek8Qg9pM",
+    authDomain: "portafolio-umg.firebaseapp.com",
+    projectId: "portafolio-umg",
+    storageBucket: "portafolio-umg.appspot.com",
+    messagingSenderId: "177586462437",
+    appId: "1:177586462437:web:de9ac3cc5f507892bebe0c",
+    measurementId: "G-J3DD5DSH7C"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
